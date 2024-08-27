71ef3ff6bddb20bf5f9bb6987134e419ac29fd0e
May Nicochelli <mayaranicochelli@gmail.com>
2024-08-27 17:40:36 -0300

    [Auth API] add initial files

---
+++ src/main/java/com/project/spring_security_auth_api/SpringSecurityAuthApiApplication.java
@@ -0,0 +1,13 @@
+package com.project.spring_security_auth_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAuthApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityAuthApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/spring_security_auth_api/SpringSecurityAuthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.spring_security_auth_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAuthApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
